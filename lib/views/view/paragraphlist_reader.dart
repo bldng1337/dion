1f4336918e89342f930ae5ffc3137a5bd4d1bbb8
bldng1337 <50496940+bldng1337@users.noreply.github.com>
2024-11-30 00:21:31 +0100

    feat(views): made paragraphlist save progress

--- lib/views/view/paragraphlist_reader.dart
+++ lib/views/view/paragraphlist_reader.dart
@@ -5,49 +5,72 @@ import 'package:dionysos/service/source_extension.dart';
 import 'package:dionysos/widgets/buttons/iconbutton.dart';
 import 'package:dionysos/widgets/buttons/textbutton.dart';
 import 'package:dionysos/widgets/scaffold.dart';
+import 'package:dionysos/widgets/selection.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dispose_scope/flutter_dispose_scope.dart';
 import 'package:go_router/go_router.dart';
 import 'package:text_scroll/text_scroll.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 final psettings = settings.readerSettings.paragraphreader;
 
-class SimpleParagraphlistReader extends StatelessWidget {
+class SimpleParagraphlistReader extends StatefulWidget {
   final SourcePath source;
   DataSource_Paragraphlist get sourcedata =>
       source.source.sourcedata as DataSource_Paragraphlist;
   const SimpleParagraphlistReader({super.key, required this.source});
 
+  @override
+  _SimpleParagraphlistReaderState createState() =>
+      _SimpleParagraphlistReaderState();
+}
+
+class _SimpleParagraphlistReaderState extends State<SimpleParagraphlistReader>
+    with StateDisposeScopeMixin {
+  late final ScrollController controller;
+
+  @override
+  void initState() {
+    final epdata = widget.source.episode.data;
+    controller = ScrollController(
+        initialScrollOffset: double.tryParse(epdata.progress ?? '0') ?? 0)
+      ..disposedBy(scope);
+    controller.addListener(
+      () {
+        epdata.progress = controller.offset.toString();
+      },
+    );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    widget.source.episode.save();
+    super.dispose();
+  }
+
   Widget wrapScreen(BuildContext context, Widget child) {
     return ListenableBuilder(
-      listenable: psettings.text.selectable,
-      builder: (context, child) {
-        if (psettings.text.selectable.value) {
-          return SelectionArea(child: child!);
-        }
-        return child!;
-      },
-      child: ListenableBuilder(
-        listenable: Listenable.merge(
-          [
-            psettings.text.linewidth,
-            psettings.text.adaptivewidth,
-          ],
-        ),
-        builder: (context, child) {
-          if (psettings.text.adaptivewidth.value &&
-              context.width < context.height) {
-            return child!;
-          }
-          final width =
-              context.width * (1 - (psettings.text.linewidth.value / 100));
-          final padding = width / 2;
-          return child!.paddingOnly(
-            left: padding,
-            right: padding,
-          );
-        },
-        child: child,
+      listenable: Listenable.merge(
+        [
+          psettings.text.linewidth,
+          psettings.text.adaptivewidth,
+        ],
       ),
+      builder: (context, child) {
+        if (psettings.text.adaptivewidth.value &&
+            context.width < context.height) {
+          return child!;
+        }
+        final width =
+            context.width * (1 - (psettings.text.linewidth.value / 100));
+        final padding = width / 2;
+        return child!.paddingOnly(
+          left: padding,
+          right: padding,
+        );
+      },
+      child: child,
     );
   }
 
@@ -55,7 +78,7 @@ class SimpleParagraphlistReader extends StatelessWidget {
     return ListenableBuilder(
       listenable: psettings.text.paragraphspacing,
       builder: (context, child) =>
-          child!.paddingOnly(bottom: psettings.text.paragraphspacing.value*2),
+          child!.paddingOnly(bottom: psettings.text.paragraphspacing.value * 5),
       child: ListenableBuilder(
         listenable: Listenable.merge([
           psettings.text.linespacing,
@@ -80,39 +103,77 @@ class SimpleParagraphlistReader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final paragraphs = sourcedata.paragraphs;
+    final epdata = widget.source.episode.data;
+    final paragraphs = widget.sourcedata.paragraphs;
     return NavScaff(
-      title: TextScroll(source.name),
+      title: TextScroll(widget.source.name),
       actions: [
         DionIconbutton(
-            icon: const Icon(Icons.settings),
-            onPressed: () => context.push('/settings/paragraphreader'),),
-      ],
-      child: wrapScreen(
-        context,
-        ListView.builder(
-          itemBuilder: (context, index) {
-            if (index == 0) {
-              return ListenableBuilder(
-                listenable: psettings.title,
-                builder: (context, child) => psettings.title.value
-                    ? Text(source.name, style: context.titleLarge)
-                    : nil,
-              );
+          icon: Icon(epdata.bookmark ? Icons.bookmark : Icons.bookmark_border),
+          onPressed: () async {
+            epdata.bookmark = !epdata.bookmark;
+            await widget.source.episode.save();
+            if (mounted) {
+              setState(() {});
             }
-            if (index-1 == paragraphs.length) {
-              if (source.episode.hasnext) {
-                return DionTextbutton(
-                  child: const Text('Next'),
-                  onPressed: () => GoRouter.of(context)
-                      .pushReplacement('/view', extra: [source.episode.next]),
+          },
+        ),
+        DionIconbutton(
+          icon: const Icon(Icons.open_in_browser),
+          onPressed: () =>
+              launchUrl(Uri.parse(widget.source.episode.episode.url)),
+        ),
+        DionIconbutton(
+          icon: const Icon(Icons.settings),
+          onPressed: () => context.push('/settings/paragraphreader'),
+        ),
+      ],
+      child: ListenableBuilder(
+        listenable: psettings.text.selectable,
+        builder: (context, child) {
+          if (psettings.text.selectable.value) {
+            return Selection(child: child!);
+          }
+          return child!;
+        },
+        child: ScrollConfiguration(
+          behavior: ScrollConfiguration.of(context).copyWith(scrollbars: false),
+          child: ListView.builder(
+            controller: controller,
+            itemBuilder: (context, index) {
+              if (index == 0) {
+                if (widget.source.episode.hasprev) {
+                  return DionTextbutton(
+                    child: const Text('Previous'),
+                    onPressed: () => widget.source.episode.goPrev(context),
+                  );
+                }
+                return nil;
+              }
+              if (index == 1) {
+                return ListenableBuilder(
+                  listenable: psettings.title,
+                  builder: (context, child) => psettings.title.value
+                      ? Text(widget.source.name, style: context.titleLarge)
+                      : nil,
                 );
               }
-              return nil;
-            }
-            return makeParagraph(context, paragraphs[index - 1]);
-          },
-          itemCount: paragraphs.length + 2,
+              if (index - 2 == paragraphs.length) {
+                if (widget.source.episode.hasnext) {
+                  return DionTextbutton(
+                    child: const Text('Next'),
+                    onPressed: () => widget.source.episode.goNext(context),
+                  );
+                }
+                return nil;
+              }
+              return wrapScreen(
+                context,
+                makeParagraph(context, paragraphs[index - 2]),
+              );
+            },
+            itemCount: paragraphs.length + 3,
+          ),
         ),
       ),
     );
