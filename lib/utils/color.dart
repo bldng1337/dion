e74b0a72afa05d3969cc2ef6aba3545cf92ece61
bldng1337 <50496940+bldng1337@users.noreply.github.com>
2024-12-09 00:39:13 +0100

    feat(utils): Added Extension on String for getting Color

--- lib/utils/color.dart
+++ lib/utils/color.dart
@@ -6,3 +6,7 @@ Color getColor(String s,{num saturation=70,num brightness=70}) {
   final hue=((s.hashCode/100)%360000)/1000;
   return HsbColor(hue, saturation, brightness);
 }
+
+extension StringColorExt on String {
+  Color get color => getColor(this);
+}
