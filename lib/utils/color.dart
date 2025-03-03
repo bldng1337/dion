8688ae4669e45d8edff88f61cfc4db8e6cacffb3
bldng1337 <50496940+bldng1337@users.noreply.github.com>
2025-03-03 12:53:17 +0100

    fix: removed flutter_color_models to fix ci

--- lib/utils/color.dart
+++ lib/utils/color.dart
@@ -1,10 +1,10 @@
-import 'dart:ui';
+import 'package:flutter/widgets.dart';
 
-import 'package:flutter_color_models/flutter_color_models.dart';
-
-Color getColor(String s,{num saturation=70,num brightness=70}) {
-  final hue=((s.hashCode/100)%360000)/1000;
-  return HsbColor(hue, saturation, brightness);
+Color getColor(String s, {num saturation = 70, num brightness = 70}) {
+  final hue = ((s.hashCode / 100) % 360000) / 1000;
+  final hsv =
+      HSVColor.fromAHSV(1, hue, saturation.toDouble(), brightness.toDouble());
+  return hsv.toColor();
 }
 
 extension StringColorExt on String {
