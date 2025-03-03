46c8f837b7271d4a4871135916c25e3891f00ae4
bldng1337 <50496940+bldng1337@users.noreply.github.com>
2025-03-03 21:53:21 +0100

    fix: normalize saturation and brightness in getColor

--- lib/utils/color.dart
+++ lib/utils/color.dart
@@ -3,7 +3,7 @@ import 'package:flutter/widgets.dart';
 Color getColor(String s, {num saturation = 70, num brightness = 70}) {
   final hue = ((s.hashCode / 100) % 360000) / 1000;
   final hsv =
-      HSVColor.fromAHSV(1, hue, saturation.toDouble(), brightness.toDouble());
+      HSVColor.fromAHSV(1, hue, saturation.toDouble()/100, brightness.toDouble()/100);
   return hsv.toColor();
 }
 
