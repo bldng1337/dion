3b1af350948b6483a5b5c2f2b7dd0c53e2fc4ce1
bldng1337 <50496940+bldng1337@users.noreply.github.com>
2025-06-15 18:04:54 +0200

    feat: make padding on badge widgets optional

--- lib/widgets/badge.dart
+++ lib/widgets/badge.dart
@@ -4,18 +4,20 @@ import 'package:flutter/material.dart';
 class DionBadge extends StatelessWidget {
   final Widget child;
   final Color? color;
-  const DionBadge({super.key, required this.child, this.color});
+  final bool noPadding;
+  const DionBadge(
+      {super.key, required this.child, this.color, this.noPadding = false});
 
   @override
   Widget build(BuildContext context) {
     return DecoratedBox(
       decoration: BoxDecoration(
         borderRadius: const BorderRadius.all(Radius.circular(4)),
-        color: color??context.theme.primaryColor,
+        color: color ?? context.theme.primaryColor,
       ),
       child: Center(
         child: child,
-      ).paddingAll(3),
+      ).paddingAll(noPadding ? 0 : 3),
     ).paddingAll(3);
   }
 }
