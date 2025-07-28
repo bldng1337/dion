8ceb911c31ae447e13e612217bcad0ccf1719bc8
bldng1337 <50496940+bldng1337@users.noreply.github.com>
2025-07-28 02:22:10 +0200

    refactor: widget getter rename and assert correct state

--- lib/widgets/dropdown/single_dropdown.dart
+++ lib/widgets/dropdown/single_dropdown.dart
@@ -3,9 +3,9 @@ import 'package:flutter/material.dart';
 class DionDropdownItem<T> {
   final T value;
   final String label;
-  DionDropdownItem({required this.value, required this.label});
+  const DionDropdownItem({required this.value, required this.label});
 
-  Widget get widget => Text(label);
+  Widget get labelWidget => Text(label);
 }
 
 class DionDropdown<T> extends StatelessWidget {
@@ -21,10 +21,15 @@ class DionDropdown<T> extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    assert(items.isNotEmpty, 'DionDropdown items cannot be empty');
+    assert(
+      value == null || items.any((item) => item.value == value),
+      'Selected value must exist in items list',
+    );
     return DropdownButton<T>(
       value: value,
       items: items
-          .map((e) => DropdownMenuItem<T>(value: e.value, child: e.widget))
+          .map((e) => DropdownMenuItem<T>(value: e.value, child: e.labelWidget))
           .toList(),
       onChanged: onChanged,
     );
