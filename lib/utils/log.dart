668930a96b7c644ac836104590f8aa4ba8167cde
bldng1337 <50496940+bldng1337@users.noreply.github.com>
2025-08-05 02:33:50 +0200

    feat: improve logging

--- lib/utils/log.dart
+++ lib/utils/log.dart
@@ -1,3 +1,34 @@
+import 'dart:collection';
+
+import 'package:flutter/foundation.dart';
 import 'package:logger/logger.dart';
 
-final logger = Logger();
+final logBuffer = MemoryOutput();
+final logger = Logger(
+  printer: HybridPrinter(
+    SimplePrinter(),
+    error: PrettyPrinter(),
+    fatal: PrettyPrinter(),
+    warning: PrettyPrinter(),
+  ),
+  filter: ProductionFilter(),
+  output: MultiOutput([ConsoleOutput(), logBuffer]),
+);
+
+class MemoryOutput extends LogOutput with ChangeNotifier {
+  final int bufferSize;
+
+  final ListQueue<OutputEvent> buffer;
+
+  MemoryOutput({this.bufferSize = 20}) : buffer = ListQueue(bufferSize);
+
+  @override
+  void output(OutputEvent event) {
+    if (buffer.length == bufferSize) {
+      buffer.removeFirst();
+    }
+
+    buffer.add(event);
+    notifyListeners();
+  }
+}
