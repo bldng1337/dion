67ecb3695af9c9268acf1167d4c18a2be6f5d332
bldng1337 <50496940+bldng1337@users.noreply.github.com>
2026-01-03 15:58:40 +0100

    feat: add mediatype episodeName and getEpisodeNames(num)

--- lib/utils/media_type.dart
+++ lib/utils/media_type.dart
@@ -16,4 +16,37 @@ extension MediaTypeExtension on MediaType {
         return Icons.help_outline;
     }
   }
+
+  String get episodeName {
+    switch (this) {
+      case MediaType.audio:
+        return 'Track';
+      case MediaType.video:
+        return 'Episode';
+      case MediaType.comic:
+        return 'Chapter';
+      case MediaType.book:
+        return 'Chapter';
+      case MediaType.unknown:
+        return 'Episode';
+    }
+  }
+
+  String getEpisodeNames(int num) {
+    if (num == 1) {
+      return episodeName;
+    }
+    switch (this) {
+      case MediaType.audio:
+        return 'Tracks';
+      case MediaType.video:
+        return 'Episodes';
+      case MediaType.comic:
+        return 'Chapters';
+      case MediaType.book:
+        return 'Chapters';
+      case MediaType.unknown:
+        return 'Episodes';
+    }
+  }
 }
